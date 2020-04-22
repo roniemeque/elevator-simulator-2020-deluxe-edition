22ebe5bc72f5e643c65072781c403d55e76ebc10
Ronie <roniemeque@icloud.com>
2020-04-22 20:47:48 -0300

    improving overall movement

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,6 +4,10 @@ import { build } from "./setup";
 const DEFAULT_FLOOR = 0;
 
 let currentFloor = DEFAULT_FLOOR;
+let moving = false;
+let direction = "up";
+let queue = [];
+let queueWatcher;
 
 const closeAllDoors = () =>
   qsAll(".door").forEach((door) => door.classList.toggle("door--open", false));
@@ -25,18 +29,49 @@ const move = async (direction: "up" | "down") => {
 };
 
 const travelTo = async (desiredFloor: number) => {
-  if (currentFloor === desiredFloor) return;
-
   closeAllDoors();
-
   if (desiredFloor > currentFloor) {
     while (desiredFloor > currentFloor) {
       await move("up");
     }
   }
-
+  if (desiredFloor < currentFloor) {
+    while (desiredFloor < currentFloor) {
+      await move("down");
+    }
+  }
   openDoorOnCurrentFloor();
+  // waiting for people to get off ?
+  await wait(2000);
 };
 
+const pushElevatorButton = (floor: number) => {
+  if (currentFloor === floor) return;
+  queue.push(currentFloor);
+};
+
+queueWatcher = setInterval(async () => {
+  if (!queue.length || moving) return;
+  moving = true;
+
+  const [nextFloor, ...remaining] = queue;
+  direction = nextFloor >= currentFloor ? "up" : "down";
+  queue = remaining;
+
+  console.log(
+    `moving from ${currentFloor} to ${nextFloor} in direction ${direction}`
+  );
+
+  // moving yada yada
+  await travelTo(nextFloor);
+  currentFloor = nextFloor;
+
+  console.log(`arrived on ${currentFloor}`);
+
+  moving = false;
+}, 500);
+
 build();
-window.travelTo = travelTo;
+
+window.goTo = pushElevatorButton;
+window.setQueue = (array = [2, 4, 2, 1, 3, 4, 2, 3]) => (queue = [...array]);
